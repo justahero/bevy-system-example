e1d5e25e608636605fb0e8a301d4cadd872936a0
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2025-02-20 12:57:04 +0100

    Expand example to get it to compile
    * add link to Github repo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 #![allow(unused_mut)]
 
+/// Reference: https://github.com/PROMETHIA-27/dependency_injection_like_bevy_from_scratch/blob/main/src/chapter2/src/more_params.rs
+
 mod scheduler;
 
 use scheduler::Scheduler;
@@ -21,45 +23,22 @@ pub trait System {
     fn call(&mut self, resources: &mut TypeMap);
 }
 
-impl<F: FnMut()> System for FunctionSystem<(), F> {
-    fn call(&mut self, _resources: &mut TypeMap) {
-        (self.f)()
-    }
-}
-
-impl<F: FnMut(T1), T1: 'static> System for FunctionSystem<(T1,), F> {
-    fn call(&mut self, resources: &mut TypeMap) {
-        let t1 = *resources
-            .remove(&TypeId::of::<T1>())
-            .unwrap()
-            .downcast::<T1>()
-            .unwrap();
-        (self.f)(t1)
-    }
-}
-impl<F: FnMut(T1, T2), T1: 'static, T2: 'static> System for FunctionSystem<(T1, T2), F> {
-    fn call(&mut self, resources: &mut TypeMap) {
-        let t1 = *resources
-            .remove(&TypeId::of::<T1>())
-            .unwrap()
-            .downcast::<T1>()
-            .unwrap();
-        let t2 = *resources
-            .remove(&TypeId::of::<T2>())
-            .unwrap()
-            .downcast::<T2>()
-            .unwrap();
-        (self.f)(t1, t2)
-    }
-}
-
 trait IntoSystem<Input> {
     type System: System;
 
     fn into_system(self) -> Self::System;
 }
 
-impl<F: FnMut()> IntoSystem<()> for F {
+impl<F: FnMut()> System for FunctionSystem<(), F> {
+    fn call(&mut self, _resources: &mut TypeMap) {
+        (self.f)()
+    }
+}
+
+impl<F: FnMut()> IntoSystem<()> for F
+where
+    for<'a, 'b> &'a mut F: FnMut(),
+{
     type System = FunctionSystem<(), Self>;
 
     fn into_system(self) -> Self::System {
@@ -70,7 +49,24 @@ impl<F: FnMut()> IntoSystem<()> for F {
     }
 }
 
-impl<F: FnMut(T1), T1: 'static> IntoSystem<(T1,)> for F {
+impl<F, T1: SystemParam> System for FunctionSystem<(T1,), F>
+where
+    for<'a, 'b> &'a mut F: FnMut(T1) + FnMut(<T1 as SystemParam>::Item<'b>),
+{
+    fn call(&mut self, resources: &mut TypeMap) {
+        fn call_inner<T1>(
+            mut f: impl FnMut(T1,), t1: T1,
+        ) {
+            f(t1);
+        }
+        call_inner(&mut self.f, T1::retrieve(resources));
+    }
+}
+
+impl<F, T1: SystemParam> IntoSystem<(T1,)> for F
+where
+    for<'a, 'b> &'a mut F: FnMut(T1) + FnMut(<T1 as SystemParam>::Item<'b>)
+{
     type System = FunctionSystem<(T1,), Self>;
 
     fn into_system(self) -> Self::System {
@@ -81,24 +77,43 @@ impl<F: FnMut(T1), T1: 'static> IntoSystem<(T1,)> for F {
     }
 }
 
-impl<F: FnMut(T1, T2), T1: 'static, T2: 'static> IntoSystem<(T1, T2)> for F {
-    type System = FunctionSystem<(T1, T2), Self>;
+trait SystemParam {
+    type Item<'new>;
 
-    fn into_system(self) -> Self::System {
-        FunctionSystem {
-            f: self,
-            marker: Default::default(),
-        }
+    fn retrieve<'r>(resources: &'r mut TypeMap) -> Self::Item<'r>;
+}
+
+struct Res<'a, T: 'static> {
+    value: &'a T,
+}
+
+impl<'a, T: 'static> Res<'a, T> {
+    pub fn inner(&self) -> &'a T {
+        self.value
     }
 }
 
-fn foo(value: i32) {
-    println!("Value is {value}");
+impl<'res, T: 'static> SystemParam for Res<'res, T> {
+    type Item<'new> = Res<'new, T>;
+
+    fn retrieve<'r>(resources: &'r mut TypeMap) -> Self::Item<'r> {
+        let value = resources
+            .get(&TypeId::of::<T>())
+            .unwrap()
+            .downcast_ref()
+            .unwrap();
+        Res { value }
+    }
+}
+
+fn foo(number: Res<i32>) {
+    println!("Value is {0}", number.inner());
 }
 
 fn main() {
     let mut scheduler = Scheduler::new();
     scheduler.add_system(foo);
     scheduler.add_resource(42i32);
+    scheduler.run();
     scheduler.run();
 }
