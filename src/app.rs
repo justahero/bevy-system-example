0f375cc645e9268ba890d2fe35853a3504cdff77
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2025-02-26 12:21:32 +0100

    Add code comments
    * re-order function

--- src/app.rs
+++ src/app.rs
@@ -11,6 +11,7 @@ use crate::{
 
 pub type StoredSystem = Box<dyn System>;
 
+/// A dummy type to represent a window related system.
 pub struct Surface {}
 
 impl IntoSystemParam for Surface {
@@ -21,18 +22,21 @@ impl IntoSystemParam for Surface {
     }
 }
 
+/// initialize a `State` object for a window.
 pub trait CreateWindowHandler {
     fn create(surface: &Surface) -> Self
     where
         Self: Sized;
 }
 
+/// Implements for empty tuple.
 impl CreateWindowHandler for () {
     fn create(_surface: &Surface) -> Self {
         ()
     }
 }
 
+/// Contains set of system functions, for now only "render".
 pub struct WindowHandlers {
     render: StoredSystem,
 }
@@ -48,6 +52,16 @@ impl WindowHandlers {
             render: Box::new(handler.into_system()),
         }
     }
+}
+
+/// Convenience function to pass in window handler functions.
+pub fn render<I, S, H>(handler: H) -> WindowHandlers
+where
+    I: 'static,
+    S: System + 'static,
+    H: IntoSystem<I, System = S>,
+{
+    WindowHandlers::render(handler)
 }
 
 #[derive(Clone, Debug)]
@@ -67,12 +81,13 @@ impl IntoSystemParam for Title {
     }
 }
 
+/// Represents information for a Window.
 pub struct WindowContext {
     /// The window title.
     title: RefCell<Title>,
     /// The state instance associated with the window
     state: RefCell<Box<dyn Any>>,
-    /// The associated surface to "render" into.
+    /// The associated surface to "render" into, for illustration.
     surface: RefCell<Surface>,
 }
 
@@ -89,15 +104,6 @@ impl WindowContext {
     pub fn state<'res>(&self) -> &RefCell<Box<dyn Any>> {
         &self.state
     }
-}
-
-pub fn render<I, S, H>(handler: H) -> WindowHandlers
-where
-    I: 'static,
-    S: System + 'static,
-    H: IntoSystem<I, System = S>,
-{
-    WindowHandlers::render(handler)
 }
 
 /// Collects all application wide fields and windows.
@@ -122,8 +128,11 @@ impl AppContext {
 
 type WindowCreateFn = Box<dyn for<'window> Fn(&Surface) -> Box<dyn Any>>;
 
+/// The main type to set up an application.
 pub struct App {
+    /// The list of window handlers to create all windows and their associated state with.
     pub windows: Vec<(WindowCreateFn, WindowHandlers)>,
+    /// The inner app context.
     pub context: AppContext,
 }
 
